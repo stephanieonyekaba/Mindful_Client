bc2fcaed256c5fed5b60f2381bce528be7fd1e34
stephanieonyekaba <onyek009@umn.edu>
2022-09-08 13:09:08 -0500

    m

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://mindfulyoga1.herokuapp.com',
+	production: 'https://mindfullyyoga.herokuapp.com',
 	development: 'http://localhost:8000',
 }
 
